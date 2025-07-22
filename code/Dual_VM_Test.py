919fc3861efd4e068227ce02707992f190af73c0
AsianaHolloway <167465369+AsianaHolloway@users.noreply.github.com>
2025-07-22 01:22:19 -0500

    Add files via upload
    This script performs the same ML pipeline as Single_VM_Test.py, but distributes computation across two virtual machines using Spark’s parallel processing capabilities. It evaluates not only model accuracy but also compares computational speed and resource efficiency across the dual-node cluster, highlighting the scalability of Spark in a distributed environment.

---
+++ code/Dual_VM_Test.py
@@ -0,0 +1,23 @@
+Python 3.12.0 (v3.12.0:0fb18b02c8, Oct  2 2023, 09:45:56) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from pyspark.sql import SparkSession
+... import time
+... 
+... spark = SparkSession.builder.appName("Employee Attrition Analysis - Dual VM").getOrCreate()
+... 
+... start_load_time = time.time()
+... df = spark.read.csv("/home/sat3812/employee_attrition/WA_Fn-UseC_-HR-Employee-Attrition.csv", header=True, inferSchema=True)
+... load_time = time.time() - start_load_time
+... print(f"Data Loading Time (Dual VM): {load_time:.2f} seconds")
+... 
+... start_clean_time = time.time()
+... df_cleaned = df.dropna()
+... clean_time = time.time() - start_clean_time
+... print(f"Data Cleaning Time (Dual VM): {clean_time:.2f} seconds")
+... 
+... start_analysis_time = time.time()
+... attrition_counts = df_cleaned.groupBy("Attrition").count()
+... attrition_counts.show()
+... analysis_time = time.time() - start_analysis_time
+... print(f"Data Analysis Time (Dual VM): {analysis_time:.2f} seconds")
+... 
